dfe53349ea505f28d5ca7e731b845ee3df405ad5
Ishita <ishitagops12@gmail.com>
2023-03-26 10:58:35 +0530

    php file added to display data

--- index.php
+++ index.php
@@ -1,56 +1,47 @@
 <html>
-    <head><title>PHP display</title></head>
-
     <body>
+        <?php
+
+        $name = $age = $comments = " " ;
+
+        if($_SERVER["REQUEST_METHOD"]=="POST")
+        {
+            $name = test_input($POST["name"]);
+            $age = test_input($POST["age"]);
+            $comments = test_input($POST["comments"]);
+
+        }
+
+        function test_input($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        ?>
+
+        <form method="post">
+
+        Username : <input type="text" name="name" >
+        Age : <input type="text" name="age" >
+        Comments : <input type="text" name="comments" >
+        <input type="submit" value="submit">
+
+    </form>
 
     <?php
 
-    $name = $age = $comments = "" ;
-
-    if($_SERVER["REQUEST_METHOD"]=="POST")
-    {
-        $name = test_input($_POST["name"]);
-        $age = test_input($_POST["age"]);
-        $comments = test_input($_POST["comments"]);
-    }
-
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-
-
-    }
+    echo "$name";
+    echo "<br>";
+    echo "$age";
+    echo "<br>";
+    echo "$comments";
+    echo "<br>";
 
     ?>
 
-    <form method="post">
-
-    Username: <input type="text" name="name">
-    Age: <input type="text" name="age">
-    Comments: <input type="text" name="comments">
-
-    <input type="submit" value="submit">
-
-</form>
-<?php
-
-
-echo "$name";
-echo "<br>";
-echo "$age";
-echo "<br>";
-echo "$comments";
-?>
-
-</body>
-</html>
-
-
-<!-- Run this:
-
-php -S localhost:8000 
-
--->
+    </body>
+    </html>
+    
